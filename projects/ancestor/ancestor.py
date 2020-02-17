1209ca273be3cc0e8ca3b17273511d3594dd75bc
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-02-16 19:39:10 -0800

    all parent child relationships in ancestor.py added into a dcitionary with the child as key and parent as value

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,29 @@
+# def helper_add_vertices():
+    
+def helper_add_edges(pair, vertices):
+    if pair[0] in vertices and pair[1] in vertices:
+        vertices[f'{pair[0]}'].add(f'{pair[1]}')
+
 
 def earliest_ancestor(ancestors, starting_node):
+    # put all the ancestor pairs in a dictionary with all the parents in the value set()
+    vertices = {}
+    for pair in ancestors:
+        print('initial pair', pair)
+        if vertices.get(f'{pair[1]}') == None:
+            # print('pair[0]',pair)
+            vertices[f'{pair[1]}'] = set(f'{pair[0]}')
+            # print('vertices after adding a new key and value pair',vertices)
+        if vertices.get(f'{pair[0]}') == None:
+            # print('pair[1]',pair)
+            vertices[f'{pair[0]}'] = set()
+            # print('vertices after adding a new key',vertices)
+        if vertices.get(f'{pair[1]}') != None and vertices.get(f'{pair[0]}') != None:
+            # print('pair[0][1]',pair)
+            vertices[f'{pair[1]}'].add(f'{pair[0]}')
+            # print('vertices after adding new value',vertices)
+    print('final vertices', vertices)
     pass
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+earliest_ancestor(test_ancestors, 1)
